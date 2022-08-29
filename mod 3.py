07dc47649074c227647936fb2066078f54d8ce80
HoldenLauer <110861876+HoldenLauer@users.noreply.github.com>
2022-08-29 14:02:16 -0500

    Delete mod 3.py

--- mod 3.py
+++
@@ -1,9 +0,0 @@
-# Assign a variable for the file to load and the path.
-file_to_load = 'Data Analysis Docs/Election_Analysis/election_results'
-# Open the election results and read the file.
-election_data = open(file_to_load, 'r')
-
-# To do: perform analysis.
-
-# Close the file.
-election_data.close()
